bcecc68c6ecf89b11093c00b154c42dc697cfcd1
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-12-07 15:03:34 -0500

    Move java home to constants

--- scripts/checkJava.sh
+++ scripts/checkJava.sh
@@ -2,14 +2,13 @@
 
 source `dirname $0`/constants.sh
 
-# Please set your JAVA_HOME before running
-JAVA_PATH=
+
 
 echo "Checking for installed Java environment"
 
 if [[ ! -d ${JAVA_PATH} ]]
 then
-    echo -e "${RED}ERROR${NC}:  Java home directory does not exist!  Please edit `pwd`, file ${0} and update JAVA_PATH to the location of your java installation directory"
+    echo -e "${RED}ERROR${NC}:  Java home directory does not exist!  Please edit `pwd`/constants.sh and update JAVA_PATH to the location of your java installation directory"
     exit 1
 fi
 
