e752a202cb757e2355632e0415011e44e51cc234
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-12-09 20:29:10 -0500

    Write java code to file for pumping

--- Package.swift
+++ Package.swift
@@ -11,13 +11,14 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/objecthub/swift-commandlinekit.git", .branch("master")),
         .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.2.0"),
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SimpleCodeToGraphPumper",
-            dependencies: ["CommandLineKit", "ShellOut"]),
+            dependencies: ["CommandLineKit", "ShellOut", "Logging"]),
         .testTarget(
             name: "SimpleCodeToGraphPumperTests",
             dependencies: ["SimpleCodeToGraphPumper"]),
--- Sources/SimpleCodeToGraphPumper/PumperMain.swift
+++ Sources/SimpleCodeToGraphPumper/PumperMain.swift
@@ -1,10 +1,29 @@
 import Foundation
+import Logging
 
 public class PumperMain {
+
+    private let logger: Logger = Logger.init(label: String(describing: PumperMain.self))
 
     public init() {}
 
     public func pumpOut(with javaSourceString: String) -> Bool {
+
+        //  Step 1:  Put together a file we can use
+        let fileManager = FileManager()
+        logger.debug("Operating in \(fileManager.currentDirectoryPath)")
+        let filePathToWrite = "\(fileManager.currentDirectoryPath)/output/pumperMain.java"
+        let filePath = URL.init(fileURLWithPath: filePathToWrite)
+
+        do {
+            try javaSourceString.write(to: filePath, atomically: true, encoding: .utf8)
+        } catch let error {
+            logger.error("Failed to write to url='\(filePath)' due to error:\n\(error)")
+            return false
+        }
+
+        logger.info("Successfully wrote working java doclet to '\(filePath)'")
+
         return false
     }
 
